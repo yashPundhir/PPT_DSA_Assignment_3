9b193afbb9d50b089eb524e1d7a79980f7c13100
yashPundhir <yash.pundhir.prof@gmail.com>
2023-06-02 23:36:20 +0530

    Ques 7 solved

--- sol.js
+++ sol.js
@@ -102,3 +102,56 @@ function find(nums) {
 	}
 	return uniqueNum;
 }
+
+/*
+
+Question 7
+
+You are given an inclusive range [lower, upper] and a sorted unique integer array
+nums, where all elements are within the inclusive range.
+
+A number x is considered missing if x is in the range [lower, upper] and x is not in
+nums.
+
+Return the shortest sorted list of ranges that exactly covers all the missing
+numbers. That is, no element of nums is included in any of the ranges, and each
+missing number is covered by one of the ranges.
+
+Example 1:
+Input: nums = [0,1,3,50,75], lower = 0, upper = 99
+Output: [[2,2],[4,49],[51,74],[76,99]]
+
+Explanation: The ranges are:
+[2,2]
+[4,49]
+[51,74]
+[76,99]
+
+*/
+
+// Solution 7
+
+function listOfRanges(lower, upper, nums) {
+	let lowerRange = 0;
+	let upperRange = 0;
+	let range = [];
+	const n = nums.length;
+	for (let i = 0; i < n; i++) {
+		if (i === 0 && nums[i] - lower !== 0) {
+			lowerRange = lower;
+			upperRange = nums[i] - 1;
+			range.push([lowerRange, upperRange]);
+		}
+		if (i === n - 1 && upper - nums[i] !== 0) {
+			lowerRange = nums[i] + 1;
+			upperRange = upper;
+			range.push([lowerRange, upperRange]);
+		}
+		if (1 <= i && i < n - 1 && nums[i + 1] - nums[i] !== 0) {
+			lowerRange = nums[i] + 1;
+			upperRange = nums[i + 1] - 1;
+			range.push([lowerRange, upperRange]);
+		}
+	}
+	return range;
+}
