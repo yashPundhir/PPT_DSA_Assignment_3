503c8059e394a5539080e3d9bb5bb8b722f4e5c2
yashPundhir <yash.pundhir.prof@gmail.com>
2023-06-02 23:50:04 +0530

    Ques 8 solved

--- sol.js
+++ sol.js
@@ -155,3 +155,29 @@ function listOfRanges(lower, upper, nums) {
 	}
 	return range;
 }
+
+/*
+
+Question 8
+
+Given an array of meeting time intervals where intervals[i] = [starti, endi],
+determine if a person could attend all meetings.
+
+Example 1:
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+
+*/
+
+// Solution 8
+
+function checkMeetings(intervals) {
+	intervals.sort((a, b) => a[0] - b[0]);
+	const n = intervals.length;
+	for (let i = 0; i < n - 1; i++) {
+		if (intervals[i][1] > intervals[i + 1][0]) {
+			return false;
+		}
+	}
+	return true;
+}
